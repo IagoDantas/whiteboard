04cedf927c0a97e5f0154d89c02e3edb2521a049
IagoDantas <iagodantas.8@gmail.com>
2024-02-26 12:57:19 -0300

    feat: add dashboard sidebar

--- app/(dashboard)/_components/sidebar/page.tsx
+++ app/(dashboard)/_components/sidebar/page.tsx
@@ -1,8 +1,10 @@
+import { NewButton } from "./new-button";
+
 const DashboardSidebar = () => {
   return (
-    <div>
-      Sidebar
-    </div>
+    <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white ">
+      <NewButton />
+    </aside>
   );
 }
 
