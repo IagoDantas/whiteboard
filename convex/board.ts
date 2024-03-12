bc3c417561af9d4cfe10de94a7d34abc705f2cbe
Iago Dantas <iagodantas.8@gmail.com>
2024-03-12 23:09:09 +0000

    fix: remove orgid on query to favorite

--- convex/board.ts
+++ convex/board.ts
@@ -122,8 +122,8 @@ export const favorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex("by_user_board_org", (q) =>
-        q.eq("userId", userId).eq("boardId", board._id).eq("orgId", args.orgId)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
@@ -164,7 +164,6 @@ export const unfavorite = mutation({
       .withIndex(
         "by_user_board",
         (q) => q.eq("userId", userId).eq("boardId", board._id)
-        // TODO: check if orgId needed
       )
       .unique();
 
