465d5c97c67793dbcf7c29e2d965c7f5a1f5b8a5
IagoDantas <iagodantas.8@gmail.com>
2024-03-13 15:46:37 -0300

    feat: remove unusual import and pass boardId to info

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useSelf } from "@/liveblocks.config"
 import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
@@ -12,14 +11,12 @@ interface CanvaProps {
 
 export function Canvas({ boardId }: CanvaProps) {
 
-    // const  = useSelf((me) => me.info)
-
 
     return (
         <main
             className="h-full w-full relative bg-neutral-100 touch-none"
         >
-            <Info />
+            <Info boardId={boardId} />
             <Participants />
             <Toolbar />
         </main>
